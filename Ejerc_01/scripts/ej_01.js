5a9ee49b95219456a29711568ead9b1c11132849
rotoforze <farmingwaresp@gmail.com>
2025-09-17 09:00:54 +0200

    ej1.1

--- Ejerc_01/scripts/ej_01.js
+++ Ejerc_01/scripts/ej_01.js
@@ -1,1 +1,7 @@
 const nombre = "Alex";
+let edad = 22;
+const tieneMascota = true;
+edad = 22;
+// tieneMascota = false;
+console.log(nombre, edad, tieneMascota);
+console.log(`${nombre} tiene ${edad} y `+(tieneMascota ? 'tiene':'no tiene')+' mascota');
