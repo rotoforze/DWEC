98428920c26fe25d3713c2d0f7d7cdfb5a8a028c
rotoforze <farmingwaresp@gmail.com>
2025-09-19 13:19:56 +0200

    ej2.1

---
+++ Ejerc_02/scripts/ej_01.js
@@ -0,0 +1,19 @@
+let numeros = [];
+for (let i = 0; i<6; i++) {
+    numeros[i] = Math.floor(Math.random()*10+1);
+};
+console.table(numeros);
+
+let dobles = [];
+numeros.map((numero) => {
+    if (!dobles.includes(numero)) dobles.push(numero);
+});
+console.table(dobles);
+
+let pares = [];
+numeros.filter((numero) => {
+    if (numero % 2 == 0) pares.push(numero);
+});
+for (const numero of pares) {
+    console.log(numero);
+}
