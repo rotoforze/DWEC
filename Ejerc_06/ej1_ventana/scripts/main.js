30e47b108518078cb40a993a4aa9031990343009
rotoforze <farmingwaresp@gmail.com>
2025-10-14 11:43:24 +0200

    ej6.1 refactorizado y ej6.2 empezado

--- Ejerc_06/ej1_ventana/scripts/main.js
+++ Ejerc_06/ej1_ventana/scripts/main.js
@@ -1,21 +1,10 @@
-function alternarVistaSegunBoton(boton) {
-    if (!boton) return;
-    
-    if (boton.parentNode.classList.contains('modal')) {
-        boton.parentNode.style.display = boton.parentNode.style.display == 'none' ? 'block' : 'none';
-    }else {
-        boton.parentNode.querySelector('.modal').style.display = boton.parentNode.querySelector('.modal').style.display == 'none' ? 'block' : 'none';
-    }
-    
-}
-function ponerEventoAlternarABotones(botones) {
-    if (!botones) return;
-    botones.forEach((boton) => {
-        boton.addEventListener('click', () => {
-            alternarVistaSegunBoton(boton);
-        });
+document.querySelectorAll('.mostarModal').forEach((boton) => {
+    boton.addEventListener('click', () => {
+        boton.nextElementSibling.style.display = 'block';
     });
-}   
-
-ponerEventoAlternarABotones(document.querySelectorAll('.mostarModal'));
-ponerEventoAlternarABotones(document.querySelectorAll('.cerrar'));
+});
+document.querySelectorAll('.cerrar').forEach((boton) => {
+    boton.addEventListener('click', () => {
+        boton.parentElement.style.display = 'none';
+    });
+});
