21bf6e24877e01e89ab6ede45812cb592b71f370
rotoforze <farmingwaresp@gmail.com>
2025-10-17 10:07:21 +0200

    ej6.9

---
+++ Ejerc_06/ej9_appDibujo/scripts/main.js
@@ -0,0 +1,14 @@
+let isDrawing = false;
+for (let i = 0; i<1600;i++) {
+    let div = document.createElement('div');
+    div.classList.add('dibujable');
+    div.addEventListener('mousedown', () => isDrawing = true);
+    div.addEventListener('mouseup', () => isDrawing = false);
+    div.addEventListener('mouseover', () => {
+        console.log(isDrawing);
+        if (isDrawing) {
+            div.style.backgroundColor = 'black'}
+        }
+    );
+    document.querySelector('.contenedor').appendChild(div);
+}
