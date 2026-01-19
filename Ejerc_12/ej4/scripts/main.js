f36d526c2f61485b619eeb2430bb3c3184a37fc7
rotoforze <farmingwaresp@gmail.com>
2026-01-19 17:33:41 +0100

    ej12.4

--- Ejerc_12/ej4/scripts/main.js
+++ Ejerc_12/ej4/scripts/main.js
@@ -1,14 +1,86 @@
 document.addEventListener('DOMContentLoaded', init);
-let idIntervaloCarga;
-async function init() {
-    animacionCarga()
 
-    if (false) {
+let idIntervaloCarga;
+let productos;
+let pedidosConDetalles;
+let pedidosSinDetalles;
+let detallesPedidos;
+
+
+async function init() {
+    animacionCarga();
+
+    await sacarDatos();
+    pedidosConDetalles = unirPedidos();
+
+    if (mostrarDatos()) {
         clearInterval(idIntervaloCarga);
         document.querySelector('.pantallaCarga').style.display = 'none';
-        document.querySelector('.app').hidden = false;
+        document.querySelector('.container').hidden = false;
     }
 
+}
+
+async function sacarDatos() {
+    try {
+        await Promise.all([
+            fetch("./data/productos.json").then(res => res.json()).then(data => productos = data),
+            fetch("./data/pedidos.json").then(res => res.json()).then(data => pedidosSinDetalles = data),
+            fetch("./data/detalles_pedido.json").then(res => res.json()).then(data => detallesPedidos = data)
+        ]);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function unirPedidos() {
+    return pedidosSinDetalles.map((pedido) => {
+        const detallesDelPedido = []
+        detallesPedidos.filter((detalle) => {
+            if (detalle.pedidoId == pedido.id) {
+                detallesDelPedido.push({
+                    "cantidad": detalle.cantidad,
+                    "precioUnitario": detalle.precioUnitario,
+                    "producto": getNombreDelProducto(detalle.productoId)
+                })
+            };
+        });
+
+        const totalDelPedido = detallesDelPedido.reduce((acumulador, detalle) => {
+            return acumulador + detalle.precioUnitario * detalle.cantidad
+        }, 0);
+
+        return {
+            ...pedido,
+            "detalles": detallesDelPedido,
+            "totalDelPedido": totalDelPedido
+        }
+    });
+}
+
+function getNombreDelProducto(id) {
+    for (const producto of productos) {
+        if (producto.id == id) return producto.nombre;
+    }
+}
+
+function mostrarDatos() {
+    for (const pedido of pedidosConDetalles) {
+        let pedidoHTML = `
+            <div class='card'>
+                <h3>#${pedido.id} <i>${pedido.fecha}</i></h3>
+                <p>Estado: ${pedido.estado}</p>
+                <p>Total: <b>${pedido.totalDelPedido}</b> €</p>
+                <hr>
+                <ul>`;
+
+        for (const detalles of pedido.detalles) {
+            pedidoHTML += `<li>${detalles.cantidad} x ${detalles.producto} - ${detalles.precioUnitario} €</li>`;
+        }
+
+        document.querySelector('.container').innerHTML += pedidoHTML + `</ul> </div >`;
+    }
+    return true;
 }
 
 function animacionCarga() {
