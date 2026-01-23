d44f41ebb20be68217f0082668b46419a28783ea
rotoforze <farmingwaresp@gmail.com>
2026-01-23 12:25:35 +0100

    16.1

--- Ejerc_16/ej1/scripts/main.js
+++ Ejerc_16/ej1/scripts/main.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', init);
 async function init() {
     const logContent = await fethcData();
     const logParsed = parseLog(logContent);
+    parseJSONToHTML(logParsed)
 }
 
 async function fethcData() {
@@ -15,7 +16,7 @@ async function fethcData() {
 
 function parseLog(log = '') {
     if (log.length < 1) return;
-    
+
     log = log.replaceAll(' ', '');
     log = log.replaceAll('\r\n', '|');
     log = log.split('|');
@@ -26,14 +27,40 @@ function parseLog(log = '') {
     for (let i = 0; canContinue; i += 4) {
         if (log[i] != undefined && log[i].includes('ID:')) {
             users.push({
-                id: log[i].slice(log[i].indexOf(':')+6, log[i].length),
-                user: log[i+1].slice(log[i+1].indexOf(':')+1, log[i+1].length),
-                consumo: log[i+2].slice(0, log[i+2].indexOf('bytes')).slice(log[i+2].indexOf(':')+1, log[i+2].length),
-                status: log[i+3].slice(log[i+3].indexOf(':')+1, log[i+3].length)
+                id: log[i].slice(log[i].indexOf(':') + 6, log[i].length),
+                user: log[i + 1].slice(log[i + 1].indexOf(':') + 1, log[i + 1].length),
+                consumoEnMB: (
+                    Number.parseFloat(
+                        log[i + 2]
+                            .slice(0, log[i + 2].indexOf('bytes'))
+                            .slice(log[i + 2].indexOf(':') + 1))
+                    / (1024 * 1024)).toFixed(2),
+                status: log[i + 3].slice(log[i + 3].indexOf(':') + 1, log[i + 3].length)
             })
 
-        }else canContinue = false;
+        } else canContinue = false;
     }
 
     return users;
+}
+
+function parseJSONToHTML(logArray = []) {
+    const contenedor = document.querySelector('tbody');
+    let consumoTotal = 0;
+    for (const log of logArray) {
+        consumoTotal += Number.parseFloat(log.consumoEnMB);
+        contenedor.innerHTML += `
+            <tr class="${log.status.includes('ERROR') ? 'error' : 'ok'}">
+                <td>#${log.id}</td>
+                <td>${log.user.toLowerCase()}</td>
+                <td>${log.consumoEnMB} MB</td>
+                <td>
+                    <abbr title="${log.status.slice(log.status.indexOf(':')+1, log.status.length)}" id="${log.status.includes('ERROR') ? 'error' : 'ok'}">
+                        ${log.status.includes('ERROR') ? log.status.slice(0, log.status.indexOf(':')) : log.status}
+                    </abbr>
+                </td>
+            </tr>
+        `;
+    }
+    document.querySelector('.consumoTotal').innerHTML = consumoTotal.toFixed(2);
 }
